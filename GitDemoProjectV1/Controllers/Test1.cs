977826dae7bbe6d8bb3db2dd9119b4d51d2d7f8a
Jamesy123Paul <ojieh.p78@gmail.com>
2024-03-31 22:41:19 +0100

    Add project files.

---
+++ GitDemoProjectV1/Controllers/Test1.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitDemoProjectV1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Test1 : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<Test1> _logger;
+
+        public Test1(ILogger<Test1> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
